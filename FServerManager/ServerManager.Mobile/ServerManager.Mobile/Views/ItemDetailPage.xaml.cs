d472440afbe8f015f031d1316cb8b4941d2d1b7f
AmirHosseinBaderan <amir0hossin0pr@gmail.com>
2021-01-04 20:25:32 +0330

    init layers

---
+++ FServerManager/ServerManager.Mobile/ServerManager.Mobile/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using ServerManager.Mobile.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace ServerManager.Mobile.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ FServerManager/ServerManager.Mobile/ServerManager.Mobile/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using ServerManager.Mobile.Models;
+using ServerManager.Mobile.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ServerManager.Mobile.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
