d0db96d7ef940f0500520abecabec90aeae48063
AmirHosseinBaderan <amir0hossin0pr@gmail.com>
2021-01-05 22:16:59 +0330

    insert Services

---
+++ FServerManager/Services.Server/Repository/ICommnadsRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Server.Repository
+{
+    public interface IPoweRepository
+    {
+        Task<int> Shutdown(int option);
+        Task<int> Restart(int option);
+        
+    }
+
+    public interface IUserControllerRepository
+    {
+        Task<int> NewUser(string userName, string password);
+        Task<int> ResetPassword(string userName, string newPassword);
+        Task<int> DeleteExistUser(string userName, string password);
+    }
+}
---
+++ FServerManager/Services.Server/Repository/IDevOpsRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Server.Repository
+{
+    public interface IDevOpsRepository
+    {
+        Task<int> InsertNewSite();
+    }
+}
